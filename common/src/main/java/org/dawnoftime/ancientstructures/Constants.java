acccd0c81977df393b953c950030c844970fcbd3
pchag <p.chagnon@orange.fr>
2025-06-29 23:48:35 +0200

    First commit 0.1 beta

---
+++ common/src/main/java/org/dawnoftime/ancientstructures/Constants.java
@@ -0,0 +1,11 @@
+package org.dawnoftime.ancientstructures;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "ancientstructures";
+	public static final String MOD_NAME = "Ancient Structures";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
